b2ff0633ea80aba09f5ca78dbe4e892acb7a366c
IraPoli <nikkapolya@gmail.com>
2023-03-14 23:03:32 +0200

    add changes

--- src/main/java/decorator/Text.java
+++ src/main/java/decorator/Text.java
@@ -15,7 +15,7 @@ public class Text extends Element {
     public int getInt() {
         int number;
         new WebDriverWait(WebDriverSingleton.getInstance(), 30).until(ExpectedConditions.visibilityOf(webElement));
-        number = Integer.parseInt(super.getText().replaceAll("₴", "").replaceAll(" ", ""));
+        number = Integer.parseInt(super.getText().replaceAll("₴", "").replaceAll(" ", "").replaceAll("/?", ""));
         return number;
 
     }
